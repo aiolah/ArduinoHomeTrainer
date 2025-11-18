d59174ce56b716c4ea91a9c14a248c486281b0ff
aiolah <58739714+aiolah@users.noreply.github.com>
2025-11-18 12:03:32 +0100

    Indentation améliorée

--- src/main.cpp
+++ src/main.cpp
@@ -36,105 +36,119 @@ void mqttCallback(char *topic, byte *payload, unsigned int length);
 // ------------------------------------------------------------
 // SETUP
 // ------------------------------------------------------------
-void setup() {
-    Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
 
-    connectToWiFi();
-    mqtt_client.setServer(mqtt_broker, mqtt_port);
-    mqtt_client.setCallback(mqttCallback);
-    connectToMQTTBroker();
+  connectToWiFi();
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+  mqtt_client.setCallback(mqttCallback);
+  connectToMQTTBroker();
 
-    // Heartbeat sensor init
-    pinMode(ledPin, OUTPUT);
+  // Heartbeat sensor init
+  pinMode(ledPin, OUTPUT);
 
-    Serial.println("Heartbeat Detektion Beispielcode.");
+  Serial.println("Heartbeat Detektion Beispielcode.");
 }
 
 // ------------------------------------------------------------
 // PRINT MAC ADDRESS
 // ------------------------------------------------------------
-String printMacAddress() {
-    byte mac[6];
-    Serial.print("MAC Address: ");
+String printMacAddress()
+{
+  byte mac[6];
+  Serial.print("MAC Address: ");
 
-    WiFi.macAddress(mac);
+  WiFi.macAddress(mac);
 
-    for (int i = 0; i < 6; i++) {
-        MAC_address += String(mac[i], HEX);
-        if (i < 5) MAC_address += ":";
-        if (mac[i] < 16) client_id += "0";
-        client_id += String(mac[i], HEX);
-    }
+  for (int i = 0; i < 6; i++)
+  {
+    MAC_address += String(mac[i], HEX);
+    if (i < 5)
+      MAC_address += ":";
+    if (mac[i] < 16)
+      client_id += "0";
+    client_id += String(mac[i], HEX);
+  }
 
-    Serial.println(MAC_address);
-    return MAC_address;
+  Serial.println(MAC_address);
+  return MAC_address;
 }
 
 // ------------------------------------------------------------
 // WIFI CONNECTION
 // ------------------------------------------------------------
-void connectToWiFi() {
-    WiFi.begin(ssid, password);
-    Serial.print("Connecting to WiFi");
+void connectToWiFi()
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
 
-    delay(3000);
-    printMacAddress();
+  delay(3000);
+  printMacAddress();
 
-    Serial.println("Connected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+  Serial.println("Connected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 // ------------------------------------------------------------
 // MQTT CONNECTION
 // ------------------------------------------------------------
-void connectToMQTTBroker() {
-    while (!mqtt_client.connected()) {
-        Serial.print("Connecting to MQTT Broker as ");
-        Serial.print(client_id.c_str());
-        Serial.println("...");
+void connectToMQTTBroker()
+{
+  while (!mqtt_client.connected())
+  {
+    Serial.print("Connecting to MQTT Broker as ");
+    Serial.print(client_id.c_str());
+    Serial.println("...");
 
-        if (mqtt_client.connect(client_id.c_str())) {
-            Serial.println("Connected to MQTT broker");
+    if (mqtt_client.connect(client_id.c_str()))
+    {
+      Serial.println("Connected to MQTT broker");
 
-            mqtt_client.subscribe(mqtt_topic1);
-            mqtt_client.subscribe(mqtt_topic2);
-            mqtt_client.subscribe(mqtt_topic3);
-            mqtt_client.subscribe(mqtt_topic4);
-            mqtt_client.subscribe(mqtt_topic5);
+      mqtt_client.subscribe(mqtt_topic1);
+      mqtt_client.subscribe(mqtt_topic2);
+      mqtt_client.subscribe(mqtt_topic3);
+      mqtt_client.subscribe(mqtt_topic4);
+      mqtt_client.subscribe(mqtt_topic5);
 
-            String message = "Hello EMQX I'm " + client_id;
-            mqtt_client.publish(mqtt_topic1, message.c_str());
-        } else {
-            Serial.print("Failed, rc=");
-            Serial.print(mqtt_client.state());
-            Serial.println(" retrying in 5 seconds...");
-            delay(5000);
-        }
+      String message = "Hello EMQX I'm " + client_id;
+      mqtt_client.publish(mqtt_topic1, message.c_str());
     }
+    else
+    {
+      Serial.print("Failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
 }
 
 // ------------------------------------------------------------
 // MQTT CALLBACK
 // ------------------------------------------------------------
-void mqttCallback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message received on topic: ");
-    Serial.println(topic);
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message received on topic: ");
+  Serial.println(topic);
 
-    Serial.print("Message: ");
-    String messageTemp;
+  Serial.print("Message: ");
+  String messageTemp;
 
-    for (int i = 0; i < length; i++) {
-        messageTemp += (char)payload[i];
-    }
+  for (int i = 0; i < length; i++)
+  {
+    messageTemp += (char)payload[i];
+  }
 
-    Serial.println(messageTemp);
-    Serial.println("-----------------------");
+  Serial.println(messageTemp);
+  Serial.println("-----------------------");
 }
 
 // ------------------------------------------------------------
@@ -142,81 +156,88 @@ void mqttCallback(char *topic, byte *payload, unsigned int length) {
 // ------------------------------------------------------------
 int rawValue;
 
-bool heartbeatDetected(int IRSensorPin, int delay) {
-    static int maxValue = 0;
-    static bool isPeak = false;
+bool heartbeatDetected(int IRSensorPin, int delay)
+{
+  static int maxValue = 0;
+  static bool isPeak = false;
 
-    bool result = false;
+  bool result = false;
 
-    rawValue = analogRead(IRSensorPin);
-    rawValue *= (1000 / delay);
+  rawValue = analogRead(IRSensorPin);
+  rawValue *= (1000 / delay);
 
-    if (rawValue * 4L < maxValue)
-        maxValue = rawValue * 0.8;
+  if (rawValue * 4L < maxValue)
+    maxValue = rawValue * 0.8;
 
-    if (rawValue > maxValue - (1000 / delay)) {
+  if (rawValue > maxValue - (1000 / delay))
+  {
 
-        if (rawValue > maxValue)
-            maxValue = rawValue;
+    if (rawValue > maxValue)
+      maxValue = rawValue;
 
-        if (!isPeak)
-            result = true;
+    if (!isPeak)
+      result = true;
 
-        isPeak = true;
-    }
-    else if (rawValue < maxValue - (3000 / delay)) {
-        isPeak = false;
-        maxValue -= (1000 / delay);
-    }
+    isPeak = true;
+  }
+  else if (rawValue < maxValue - (3000 / delay))
+  {
+    isPeak = false;
+    maxValue -= (1000 / delay);
+  }
 
-    return result;
+  return result;
 }
 
 // ------------------------------------------------------------
 // LOOP
 // ------------------------------------------------------------
-void loop() {
-    if (!mqtt_client.connected())
-        connectToMQTTBroker();
+void loop()
+{
+  if (!mqtt_client.connected())
+    connectToMQTTBroker();
 
-    mqtt_client.loop();
+  mqtt_client.loop();
 
-    unsigned long currentTime = millis();
-    static int beatMsec = 0;
-    int heartRateBPM = 0;
+  unsigned long currentTime = millis();
+  static int beatMsec = 0;
+  int heartRateBPM = 0;
 
-    // Publish MAC periodically
-    if (currentTime - lastPublishTime >= 10000) {
-        lastPublishTime = currentTime;
+  // Publish MAC periodically
+  if (currentTime - lastPublishTime >= 10000)
+  {
+    lastPublishTime = currentTime;
 
-        String MAC = printMacAddress();
-        String MACmsg = "Adresse MAC " + MAC;
+    String MAC = printMacAddress();
+    String MACmsg = "Adresse MAC " + MAC;
 
-        String MAC_OIIA = "OIIA ᓚᘏᗢ MAC Address : " + printMacAddress();
+    String MAC_OIIA = "OIIA ᓚᘏᗢ MAC Address : " + printMacAddress();
 
-        // Uncomment if needed
-        // mqtt_client.publish(mqtt_topic3, MACmsg.c_str());
-        // mqtt_client.publish(mqtt_topic4, "Miaou");
-    }
+    // Uncomment if needed
+    // mqtt_client.publish(mqtt_topic3, MACmsg.c_str());
+    // mqtt_client.publish(mqtt_topic4, "Miaou");
+  }
 
-    // Heartbeat detection
-    if (heartbeatDetected(analogPin, delayMsec)) {
-        heartRateBPM = 60000 / beatMsec;
+  // Heartbeat detection
+  if (heartbeatDetected(analogPin, delayMsec))
+  {
+    heartRateBPM = 60000 / beatMsec;
 
-        digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH);
 
-        String msgCoeur = String(heartRateBPM);
-        mqtt_client.publish(mqtt_topic5, msgCoeur.c_str());
+    String msgCoeur = String(heartRateBPM);
+    mqtt_client.publish(mqtt_topic5, msgCoeur.c_str());
 
-        Serial.print("Puls erkannt: ");
-        Serial.println(heartRateBPM);
+    Serial.print("Puls erkannt: ");
+    Serial.println(heartRateBPM);
 
-        beatMsec = 0;
-    }
-    else {
-        digitalWrite(ledPin, LOW);
-    }
+    beatMsec = 0;
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
 
-    delay(delayMsec);
-    beatMsec += delayMsec;
+  delay(delayMsec);
+  beatMsec += delayMsec;
 }
